3e0760786ab073093f1f2bbb5c0c6884405bf2db
AKASHCHAURASIA <akashchaurasia9336@gmail.com>
2019-07-03 16:26:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<script src='jquery-3.4.1.min.js'></script>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
 
 <script>
 // ip look up for public ip infomation
